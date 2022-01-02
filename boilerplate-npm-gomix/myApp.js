5fadf01575c3b98b8c68dad184bc3ab51bebce34
Shareef-deen <49625061+Shareef-deen@users.noreply.github.com>
2022-01-02 15:05:29 +0000

    Update myApp.js

--- boilerplate-npm-gomix/myApp.js
+++ boilerplate-npm-gomix/myApp.js
@@ -1,21 +1,27 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 console.log("Hello world");
-/*app.get("/", function(req,res){
-res.send("Hello Express");
+ 
+/*app.get('/', function(req,res){
+  res.send("Hello Express");
 }
-) 
+)
 */
+
 app.get('/',(req,res)=>{
 res.sendFile(__dirname +"/views/index.html");
-})
+});
+
+ 
+console.log("message");
 app.get("/json",(req,res)=>{
- res.json({"message": "Hello json"});
+  if(process.env.MESSAGE_STYLE == "uppercase"){
+  res.json({"message": "HELLO JSON"});
+  }
+  else{res.json({"message": "Hello json"});}
 });
 app.use("/public",express.static(__dirname + "/public"));
-
 
 
 
