ae0a020c86f0a99e44a5ec71e0e1caff81c3cc49
Shareef-deen <49625061+Shareef-deen@users.noreply.github.com>
2022-01-02 15:03:39 +0000

    Update myApp.js

--- boilerplate-npm-gomix/myApp.js
+++ boilerplate-npm-gomix/myApp.js
@@ -12,10 +12,7 @@ app.get('/',(req,res)=>{
 res.sendFile(__dirname +"/views/index.html");
 })
 app.get("/json",(req,res)=>{
-  if(process.env.MESSAGE_STYLE === "uppercase"){
-  res.json({"message": "HELLO JSON"});
-  }
-  else{res.json({"message": "Hello json"});}
+ res.json({"message": "Hello json"});
 });
 app.use("/public",express.static(__dirname + "/public"));
 
