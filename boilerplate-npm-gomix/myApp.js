eb4717bd276af680afd073d6489bdad148e2c4a4
Shareef-deen <49625061+Shareef-deen@users.noreply.github.com>
2022-01-02 15:56:40 +0000

    Update myApp.js

--- boilerplate-npm-gomix/myApp.js
+++ boilerplate-npm-gomix/myApp.js
@@ -12,12 +12,24 @@ app.get('/',(req,res)=>{
 res.sendFile(__dirname +"/views/index.html");
 });
 
- 
+/* 
 console.log("message");
 app.get("/json",(req,res)=>{
   if(process.env.MESSAGE_STYLE == "uppercase"){
   res.json({"message": "Hello json"});
 });
+ */
+ let dataJson = {"message": "Hello json"};
+ 
+app.get("/json", (req, res) => {
+ if (process.env.MESSAGE_STYLE === "uppercase") {
+   dataJson.message = dataJson.message.toUpperCase();
+ };
+ else{dataJson.message = dataJson.message.toLowerCase();}
+ 
+ res.json(dataJson);
+})
+ 
 app.use("/public",express.static(__dirname + "/public"));
 
 
